63c4c512a2bf1c91774642603763f0367d3b83f5
root <root@web.unlockededu.org>
2019-05-02 14:47:47 +0000

    adding production code. probably will have to tweak later

--- unlockededu/unlockededu/settings.py
+++ unlockededu/unlockededu/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', None)
+SECRET_KEY = None
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "www.unlockededu.org",
+    "unlockededu.org",
+]
 
 
 # Application definition
@@ -136,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
--- unlockededu/unlockededu/wsgi.py
+++ unlockededu/unlockededu/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'unlockededu.settings')
+
+sys.path.append('/var/www/website/unlockededu')
+sys.path.append('/var/www/website/unlockededu/unlockededu')
 
 application = get_wsgi_application()
